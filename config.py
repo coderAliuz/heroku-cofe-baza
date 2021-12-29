f0f8f2d5cd2bcd441d2db6f8f880214e19f215e5
Xxuligan01 <67776632+Xxuligan01@users.noreply.github.com>
2021-12-29 15:53:18 +0500

    Add database connection

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 token="902977727:AAFmFgPLE90eSINkl95WHhO3yYLG0ppbVGM"
 #app_api="https://cofebaza.herokuapp.com/"+token
+db_baza="postgres://bvvcyvzsqnpvsw:6973fd20ccb7a478d4fb749c5cfcfa259384abaf6df0dab7d8c4b70dccbc5830@ec2-52-211-233-176.eu-west-1.compute.amazonaws.com:5432/d7tq4l3cpiu32k"
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from aiogram.dispatcher.filters import Text
 from aiogram.types import chat, message
 from config import *
 from flask import Flask, request
-
+import psycopg2
+db_con=psycopg2.connect(db_baza,sslmode="require")
+db_ob=db_con.cursor()
 
 
 logging.basicConfig(level=logging.INFO)
@@ -14,6 +16,16 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start','restart'])
 async def send_welcome(message: types.Message):
     await message.reply(f"🤓Assalom aleykum✋\n{message.chat.full_name}\n🥤GulDU Coffee Clubga\n😍XUSH KELDINGIZ‼️")
+    user_id=message.chat.id
+    name=message.chat.full_name
+    db_ob.execute(f"SELECT id FROM cofe_baza where id={message.chat.id}")
+    data = db_ob.fetchone()
+    if data is None:
+        db_ob.execute('INSERT INTO cofe_baza VALUES (?,?)', (user_id, name))
+        db_ob.commit()
+    else:
+        await bot.delete_message(message.chat.id, message.message_id)
+        await message.answer("🆘 Ushbu raqam foydalanuvchi raqamiga mos kelmadi❌❗️")
 
 if __name__=='__main__':
     executor.start_polling(dp, skip_updates=True)
